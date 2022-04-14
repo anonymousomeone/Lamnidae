91037c698b9b5e20d119a36dfbd350daa7fd5bf9
anonymousomeone <leoistheworst22@gmail.com>
2022-04-13 23:29:03 -0700

    connection success!!!

---
+++ index.js
@@ -0,0 +1,32 @@
+var WebSocketClient = require('websocket').client;
+
+var client = new WebSocketClient();
+
+client.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
+});
+
+client.on('connect', function(connection) {
+    console.log('WebSocket Client Connected');
+    connection.on('error', function(error) {
+        console.log("Connection Error: " + error.toString());
+    });
+    connection.on('close', function() {
+        console.log('echo-protocol Connection Closed');
+    });
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log("Received: '" + message.utf8Data + "'");
+        }
+    });
+});
+
+const opts = {
+        host: "pixelplace.io",
+        connection: "Upgrade",
+        upgrade: "websocket",
+        origin: "https://pixelplace.io",
+}
+const usragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36"
+
+client.connect('wss://pixelplace.io/socket.io/?EIO=3&transport=websocket', 'echo-protocol', null, opts, opts);
