da2b9df97180bed99c605fb2f2b0085b51dbf4e6
anonymousomeone <leoistheworst22@gmail.com>
2022-04-19 14:08:44 -0700

    working, color dist compare stupid though

--- index.js
+++ index.js
@@ -87,53 +87,6 @@ function place(x, y, color) {
 }
 
 function parseImage(img) {
-    // uncomment commented colors if you have premium
-    // TODO: move this to config json
-    var colors = {
-        // shades of white, white => black
-        "0": (255, 255, 255),
-        "1": (196, 196, 196),
-        "2": (136, 136, 136),
-        "3": (85, 85, 85),
-        "4": (34, 34, 34),
-        "5": (0, 0, 0),
-
-        // colors
-        "6": (0, 102, 0),
-        "7": (34, 177, 76),
-        "8": (2, 190, 1),
-        // "9": (81, 255, 25),
-        "10": (148, 224, 68),
-        "11": (251, 255, 91),
-        "12": (229, 217, 0),
-        "13": (230, 190, 12),
-        "14": (229, 149, 0),
-        "15": (160, 106, 66),
-        "16": (153, 83, 13),
-        "17": (99, 60, 31),
-        "18": (107, 0, 0),
-        "19": (159, 0, 0),
-        "20": (229, 0, 0),
-        // "21": (255, 57, 4),
-        "22": (187, 79, 0),
-        "23": (255, 117, 95),
-        "24": (255, 196, 159),
-        "25": (255, 223, 204),
-        "26": (255, 167, 209),
-        "27": (207, 110, 228),
-        "28": (236, 8, 236),
-        "29": (130, 0, 128),
-        // discord blurple 🤢🤮
-        // "30": (81, 0, 255),
-        "31": (2, 7, 99),
-        "32": (0, 0, 234),
-        "33": (4, 75, 255),
-        "34": (101, 131, 207),
-        "35": (54, 186, 255),
-        "36": (0, 131, 199),
-        "37": (0, 211, 221),
-        // "38": (69, 255, 200)
-    }
     console.log('reading')
     Jimp.read(img, (err, image) => {
         image.scan(0, 0, image.bitmap.width, image.bitmap.height, function(x, y, idx) {
@@ -141,10 +94,85 @@ function parseImage(img) {
             var red = this.bitmap.data[idx + 0];
             var green = this.bitmap.data[idx + 1];
             var blue = this.bitmap.data[idx + 2];
-           
-            console.log(red)
+            var rgb = findColor([red, green, blue])
+
+            image.setPixelColor(Jimp.rgbaToInt(rgb[0], rgb[1], rgb[2], 255), x, y)
           });
+        image.write('lena-small-bw.jpg')
     })
+    console.log('done')
 }
 
+function findColor(rgb) {
+    // TODO: move this to config json
+    var colors = {
+        // shades of white, white => black
+        0:  [255, 255, 255],
+        1:  [196, 196, 196],
+        2:  [136, 136, 136],
+        3:  [85, 85, 85],
+        4:  [34, 34, 34],
+        5:  [0, 0, 0],
+
+        // color
+        6:  [0, 102, 0],
+        7:  [34, 177, 76],
+        8:  [2, 190, 1],
+        9:  [148, 224, 68],
+        10: [251, 255, 91],
+        11: [229, 217, 0],
+        12: [230, 190, 12],
+        13: [229, 149, 0],
+        14: [160, 106, 66],
+        15: [153, 83, 13],
+        16: [99, 60, 31],
+        17: [107, 0, 0],
+        18: [159, 0, 0],
+        19: [229, 0, 0],
+        20: [187, 79, 0],
+        21: [255, 117, 95],
+        22: [255, 196, 159],
+        23: [255, 223, 204],
+        24: [255, 167, 209],
+        25: [207, 110, 228],
+        26: [236, 8, 236],
+        27: [130, 0, 128],
+        28: [2, 7, 99],
+        29: [0, 0, 234],
+        30: [4, 75, 255],
+        31: [101, 131, 207],
+        32: [54, 186, 255],
+        33: [0, 131, 199],
+        34: [0, 211, 221],
+    }
+
+    var closest = 765
+    var res = []
+    for (var i = 0; i < Object.keys(colors).length; i++) {
+        // console.log(colors[i])
+        var d =   ((colors[i][0]-rgb[0])*0.30)^2
+                + ((colors[i][1]-rgb[1])*0.59)^2
+                + ((colors[i][2]-rgb[2])*0.11)^2
+        // console.log(`${colors[i]}: ${d}`)
+
+        // color dist comparing might be buggy
+        if (d < closest && !(d < 0)) { 
+            closest = d
+            res = colors[i]
+        }
+    }
+    return res
+}
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function toHex(rgb) {
+    console.log(rgb)
+    return `#${componentToHex(rgb[0])}${componentToHex(rgb[1])}${componentToHex(rgb[2])}`
+}
+
+// findColor([50, 100, 100])
 parseImage('test.jpg')
